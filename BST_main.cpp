9b11dbf599f1a068c08e138864004c74dba30ae4
JohnBlue-git <83207141+JohnBlue-git@users.noreply.github.com>
2022-01-14 21:36:50 +0800

    void Del(...) {...} and ~BST() {pt = 0}

--- BST_main.cpp
+++ BST_main.cpp
@@ -58,7 +58,7 @@ public:
   int MaxK();
   //int floor(int ky);
   //int ceil(int ky);
-  //void delete();
+  void Del(int ky);
 };
 
 BST::BST(const BST& copy): key(copy.key), data(copy.data), left(0), right(0) {
@@ -80,31 +80,34 @@ BST::~BST() {
   if (left != 0) {
     (*left).BST::~BST();
     left = 0;
+    // delete null will not doing anything, but it will prevent to delete next ...
+    delete left;
     //std::cout << "left\n";
   }
   // right
   if (right != 0) {
     (*right).BST::~BST();
     right = 0;
+    // delete null will not doing anything, but it will prevent to delete next ...
+    delete right;
     //std::cout << "right\n";
   }
 }
 
 void BST::print() {
-  BST* current = this;
   // checks
-  if (current->key == 0) {
+  if (key == 0) {
     return;
   }
   // middle
-  std::cout << "key" << current->key << ": " << current->data << "\n";
+  std::cout << "key" << key << ": " << data << "\n";
   // left
   if (left != 0) {
-    current->left->print();
+    left->print();
   }
   // right
   if (right != 0) {
-    current->right->print();
+    right->print();
   }
 }
 
@@ -165,37 +168,80 @@ int BST::MaxK() {
   return current->data;
 }
 
+// case:
 // no kid
 // one kid
 // two kids
-/*
-void BST::delete(int ky) {
+void BST::Del(int ky) {
+  int l_r = 1;// left 0 right 2
   BST* p_current = 0;
   BST* current = this;
   while (current != 0) {
-    p_current = current;
+    // search
     if (ky < current->key) {
+      p_current = current;
+      l_r = 0;
       current = current->left;
     }
     else if (ky > current->key) {
+      p_current = current;
+      l_r = 2;
       current = current->right;
     }
+    // delete
     else if (ky == current->key) {
       if (current->left == 0 && current->right == 0) {
-        delete current;
-        p_current = 0;
+        // no need ?
+        if (l_r == 0) {
+          p_current->left = 0;
+        }
+        else if (l_r == 2) {
+          p_current->right = 0;
+        }
+        //delete current;// not work @@
+        current->~BST();
         return;
       }
-      else if (current->left == 0 || current->right == 0) {
-        delete current;
+      else if (current->left != 0 && current->right == 0) {
+        if (l_r == 0) {
+          p_current->left = current->left;
+        }
+        else if (l_r == 2) {
+          p_current->right = current->left;
+        }
+        current->left = 0;
+        delete current;//current->~BST();
+        return;
+      }
+      else if (current->left == 0 && current->right != 0) {
+        if (l_r == 0) {
+          p_current->left = current->right;
+        }
+        else if (l_r == 2) {
+          p_current->right = current->right;
+        }
+        current->right = 0;
+        delete current;//current->~BST();
+        return;
+      }
+      else {
+        if (current->right->left != 0) {
+          current->key = current->right->left->key;
+          current->data = current->right->left->data;
+          current->right->left->Del(current->right->left->key);
+        }
+        else if (current->right != 0) {
+          current->key = current->right->key;
+          current->data = current->right->data;
+          current->right->Del(current->right->key);
+        }
         return;
       }
     }
   }
   // not found
-  ...
+  std::cout << "no such key\n";
 }
-*/
 
 int main()
 {
@@ -215,9 +261,17 @@ int main()
   bt.print();
   std::cout << "\n";
   // get
-  std::cout << "key(8):" << bt.get(8) << "\n";
-  std::cout << "key(9):" << bt.get(9) << "\n";
+  std::cout << "get key(8):" << bt.get(8) << "\n";
+  std::cout << "get key(9):" << bt.get(9) << "\n\n";
   // max key vale
-  std::cout << "Max Key(15):" << bt.MaxK() << "\n";
+  std::cout << "Max Key:" << bt.MaxK() << "\n\n";
   // delete
+  std::cout << "Del(15):\n";
+  bt.Del(15);
+  std::cout << "Del(14):\n";
+  bt.Del(14);
+  std::cout << "Del(12):\n";
+  bt.Del(12);
+  bt.print();
+  std::cout << "\n";
 }
